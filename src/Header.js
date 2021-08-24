da6dd6d77af338b27326e9ebf0665a187aab183b
chathura <chathuraperera007@gmail.com>
2021-08-24 14:57:15 +0530

    Added routing

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,14 @@ import { ExpandMore } from "@material-ui/icons/";
 import { Search } from "@material-ui/icons/";
 import { Language } from "@material-ui/icons/";
 import { Avatar } from "@material-ui/core";
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
+      <Link to='/'>
       <img src={logo} className="header__icon" alt="Airbnb logo" />
+      </Link>
       <div className="header__center">
         <input type="text" name="" id="" />
         <Search />
