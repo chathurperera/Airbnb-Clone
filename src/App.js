30a2a20d62156f01a1b9ebc43a6b9dbaa44a3663
chathura <chathuraperera007@gmail.com>
2021-08-24 14:28:34 +0530

    Added the search page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Home from "./Home";
 import Header from "./Header";
-import './App.css';
+import "./App.css";
 import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
     <div className="app">
+      <Router>
         <Header />
-        <Home />
-        <Footer />
 
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </Router>
     </div>
   );
 }
