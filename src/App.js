02a727f9b58ed71cb438bb3e36265b5d1336e816
chathura <chathuraperera007@gmail.com>
2021-08-24 11:53:48 +0530

    Added the card components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./Home";
 import Header from "./Header";
 import './App.css';
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="app">
         <Header />
         <Home />
+        <Footer />
 
     </div>
   );
